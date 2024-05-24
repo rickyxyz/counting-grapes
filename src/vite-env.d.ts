e36e168ed3eeff9f456170c3a751f262d87b711e
rickyxyz <56119701+rickyxyz@users.noreply.github.com>
2024-05-25 00:51:28 +0700

    initial commit

---
+++ src/vite-env.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="vite/client" />
+interface ImportMetaEnv {
+  readonly VITE_API_KEY: string;
+  readonly VITE_PROJECT_ID: string;
+  readonly VITE_APP_ID: string;
+  readonly VITE_RTDB_URL: string;
+}
